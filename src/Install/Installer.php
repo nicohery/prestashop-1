c7f75b2d7e9422c94cd8ac5bc551964845b86c6c
Nico H <nico.hery@gmail.com>
2026-07-07 14:47:25 +0200

    feat: configurable cart footer (displayShoppingCartFooter) with editable messages and alerts

--- src/Install/Installer.php
+++ src/Install/Installer.php
@@ -94,7 +94,12 @@ class Installer
             && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_ORDER_THREAD_STATUS, 'open')
             && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_FREQUENCY_PRICE_BASE, 'gross')
             && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_ENABLE_CREATION_ORDER_STATE, '0')
-            && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_CREATION_ORDER_STATE, '0');
+            && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_CREATION_ORDER_STATE, '0')
+            // Récap panier : footer + alertes activés par défaut ; messages laissés
+            // vides (le template retombe sur des textes traduisibles par défaut).
+            && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_CART_FOOTER_ENABLED, '1')
+            && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_CART_ALERT_MIXED_ENABLED, '1')
+            && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_CART_ALERT_FREQ_ENABLED, '1');
     }
 
     /**
@@ -128,6 +133,12 @@ class Installer
             \Ciklik::CONFIG_DELEGATE_OPTIONS_DISPLAY,
             \Ciklik::CONFIG_ENABLE_CREATION_ORDER_STATE,
             \Ciklik::CONFIG_CREATION_ORDER_STATE,
+            \Ciklik::CONFIG_CART_FOOTER_ENABLED,
+            \Ciklik::CONFIG_CART_FOOTER_MESSAGE,
+            \Ciklik::CONFIG_CART_ALERT_MIXED_ENABLED,
+            \Ciklik::CONFIG_CART_ALERT_MIXED_MESSAGE,
+            \Ciklik::CONFIG_CART_ALERT_FREQ_ENABLED,
+            \Ciklik::CONFIG_CART_ALERT_FREQ_MESSAGE,
         ];
 
         foreach ($keys as $key) {
@@ -256,6 +267,7 @@ class Installer
             'displayAdminProductsExtra',
             'actionCartUpdateQuantityBefore',
             'displayShoppingCart',
+            'displayShoppingCartFooter',
             'displayCartExtraProductInfo',
             'actionAuthentication',
             'actionProductUpdate',
